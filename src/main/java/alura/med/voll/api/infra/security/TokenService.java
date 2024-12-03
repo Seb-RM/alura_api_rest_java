0b5eff69f97ac57020d5d31a475e178ec9b424c4
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-12-03 01:21:57 -0300

    autencicacion jwt filtro

--- src/main/java/alura/med/voll/api/infra/security/TokenService.java
+++ src/main/java/alura/med/voll/api/infra/security/TokenService.java
@@ -2,8 +2,11 @@ package alura.med.voll.api.infra.security;
 
 import alura.med.voll.api.model.Usuario;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -34,5 +37,37 @@ public class TokenService {
 
     private Instant generarFechaExpiracion(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
+    }
+
+    public String getSubject(String token) {
+
+//        DecodedJWT verifier = null;
+//        try {
+//        Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+//            verifier = JWT.require(algorithm)
+//                    // specify any specific claim validations
+//                    .withIssuer("voll med")
+//                    // reusable verifier instance
+//                    .build()
+//                    .verify(token);
+//            verifier.getSubject();
+//        } catch (JWTVerificationException exception){
+//            System.out.println(exception.toString());
+//
+//        }
+//        if(verifier.getSubject()== null) {
+//            throw new RuntimeException("Verifier inválido");
+//        }
+//        return verifier.getSubject();
+        try {
+            var algoritmo = Algorithm.HMAC256(apiSecret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido o expirado!");
+        }
     }
 }
